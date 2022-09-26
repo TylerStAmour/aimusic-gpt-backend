a1f04b53dbd3388f3122f495146655c173db3fcf
Tyler St-Amour <tylerstamour1@gmail.com>
2022-09-26 00:50:58 -0400

    Simplify code to avoid dealing with annoying imports at 1AM

--- main.go
+++ main.go
@@ -1,17 +1,70 @@
 package main
 
 import (
-	"aimusic_gpt_backend/router"
+	"bytes"
+	"fmt"
+	"github.com/gin-gonic/gin"
+	"io/ioutil"
 	"net/http"
+	"os"
 )
 
 func main() {
 	server := &http.Server{
 		Addr: "http://127.0.0.1:8080",
-		Handler: router.Get(),
+		Handler: Get(),
 	}
 
 	if err := server.ListenAndServe(); err != nil {
 		panic(err)
 	}
+}
+
+const Endpoint = "https://api.openai.com/v1/completions"
+
+func Get() *gin.Engine {
+	router := gin.New()
+
+	gpt := router.Group("/api/gpt")
+	gpt.POST("/prompt", postGPTPrompt)
+
+	gpt.Use(setAccessControl())
+	return router
+}
+
+func setAccessControl() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		c.Header("Access-Control-Allow-Origin", "https://aimusic.ca")
+		c.Header("Access-Control-Allow-Credentials", "true")
+		c.Header("Access-Control-Allow-Methods", "POST")
+	}
+}
+
+func postGPTPrompt(ctx *gin.Context) {
+	prompt := ctx.Query("prompt")
+
+	payload := fmt.Sprintf("\"model\": \"text-babbage-001\", \"prompt\": \"%s\", \"temperature\": 0, \"max_tokens\": 512}", prompt)
+	request, err := http.NewRequest("POST", Endpoint, bytes.NewBufferString(payload))
+	if err != nil {
+		ctx.AbortWithError(http.StatusInternalServerError, err)
+		return
+	}
+
+	request.Header.Set("Content-Type", "application/json")
+	request.Header.Set("Authorization", "Bearer " + os.Getenv("OPENAI_ACCESS_KEY"))
+
+	client := &http.Client{}
+	resp, err := client.Do(request)
+	if err != nil {
+		ctx.AbortWithError(http.StatusInternalServerError, err)
+		return
+	}
+
+	data, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		ctx.AbortWithError(http.StatusInternalServerError, err)
+		return
+	}
+
+	ctx.JSON(http.StatusOK, data)
 }
